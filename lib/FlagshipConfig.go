d05f00ba4cc1f4934de0b83b74a4140993ec3d8f
Heihokon <neoshark22@gmail.com>
2022-04-26 13:01:16 +0100

    refactor FlagshipConfig test

--- lib/FlagshipConfig.go
+++ lib/FlagshipConfig.go
@@ -3,26 +3,22 @@ package lib
 import (
 	"flag"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 const (
 	FS_ENV_ID                 = "FS_ENV_ID"
 	FS_POLLING_INTERVAL       = "FS_POLLING_INTERVAL"
-	FS_BUCKETING_DIRECTORY    = "FS_BUCKETING_DIRECTORY"
 	FS_PORT                   = "FS_PORT"
 	FS_ADDRESS                = "FS_ADDRESS"
 	FlagshipEnvIdErrorMessage = "argument envId is required"
-	BucketingDirectoryError   = "environment variable bucketingPath is empty or not set, default value will be used"
 	DEFAULT_PORT              = 8080
 	DEFAULT_ADDRESS           = "0.0.0.0"
+	DEFAULT_POLLING_INTERVAL  = 2000
 )
 
 type FlagshipConfig struct {
 	EnvId           string `json:"envId"`
 	PollingInterval int    `json:"pollingInterval"`
-	BucketingPath   string `json:"bucketingPath"`
 	Port            int    `json:"port"`
 	Address         string `json:"address"`
 }
@@ -30,89 +26,31 @@ type FlagshipConfig struct {
 func (flagshipConfig *FlagshipConfig) New() (*FlagshipConfig, error) {
 
 	envId := flag.String("envId", "", "environment Id")
-
 	port := flag.Int("port", DEFAULT_PORT, "Http endpoint port. Default: 8080")
-	address := flag.String("address", "0.0.0.0", "Http endpoint address. Default: 0.0.0.0")
-
-	pollingInterval := flag.Int("pollingInterval", -1, "pollingInterval")
-
-	bucketingDirectory := flag.String("bucketingPath", "", "bucketing Directory path")
+	address := flag.String("address", DEFAULT_ADDRESS, "Http endpoint address. Default: 0.0.0.0")
+	pollingInterval := flag.Int("pollingInterval", DEFAULT_POLLING_INTERVAL, "pollingInterval")
 
 	flag.Parse()
 
-	if *envId != "" {
-		_ = os.Setenv(FS_ENV_ID, *envId)
-	}
-
-	if *port != DEFAULT_PORT {
-		_ = os.Setenv(FS_PORT, strconv.Itoa(*port))
-	}
-
-	if *address != DEFAULT_ADDRESS {
-		_ = os.Setenv(FS_ADDRESS, *address)
-	}
-
-	if *pollingInterval > -1 {
-		_ = os.Setenv(FS_POLLING_INTERVAL, strconv.Itoa(*pollingInterval))
-	}
-
-	if *bucketingDirectory != "" {
-		_ = os.Setenv(FS_BUCKETING_DIRECTORY, *bucketingDirectory)
-	}
-
-	return flagshipConfig.GetConfig()
-}
-
-func (flagshipConfig *FlagshipConfig) GetConfig() (*FlagshipConfig, error) {
-
-	envId := os.Getenv(FS_ENV_ID)
-	if envId == "" {
+	if *envId == "" {
 		return nil, fmt.Errorf(FlagshipEnvIdErrorMessage)
 	}
+	flagshipConfig.EnvId = *envId
 
-	flagshipConfig.EnvId = envId
-
-	bucketingDirectory := os.Getenv(FS_BUCKETING_DIRECTORY)
-
-	if bucketingDirectory == "" {
-		fmt.Println(BucketingDirectoryError)
-	}
-
-	flagshipConfig.BucketingPath = bucketingDirectory
-
-	envPollingInterval := os.Getenv(FS_POLLING_INTERVAL)
-
-	if envPollingInterval == "" {
-		flagshipConfig.PollingInterval = 2000
-		fmt.Println("argument pollingInterval is empty or not set, default value will be used 2000ms")
-	} else {
-		pollingInterval, err := strconv.Atoi(envPollingInterval)
-		if err != nil {
-			pollingInterval = 2000
-			fmt.Printf("argument pollingInterval is not an int, default value will be used 2000ms")
-		}
-		flagshipConfig.PollingInterval = pollingInterval
-	}
-
-	envPort := os.Getenv(FS_PORT)
-	if envPort == "" {
-		flagshipConfig.Port = DEFAULT_PORT
+	if *port == DEFAULT_PORT {
 		fmt.Println("argument port is empty or not set, default value will be used 8080")
-	} else {
-		port, err := strconv.Atoi(envPort)
-		if err != nil {
-			port = DEFAULT_PORT
-			fmt.Printf("argument port is not an int, default value will be used 8080")
-		}
-		flagshipConfig.Port = port
 	}
+	flagshipConfig.Port = *port
 
-	address := os.Getenv(FS_ADDRESS)
-	if address == "" {
-		address = DEFAULT_ADDRESS
+	if *address == DEFAULT_ADDRESS {
 		fmt.Println("argument address is empty or not set, default value will be used 0.0.0.0")
 	}
-	flagshipConfig.Address = address
+	flagshipConfig.Address = *address
+
+	if *pollingInterval == DEFAULT_POLLING_INTERVAL {
+		fmt.Println("argument pollingInterval is empty or not set, default value will be used 2000ms")
+	}
+	flagshipConfig.PollingInterval = *pollingInterval
 
 	return flagshipConfig, nil
 }
--- lib/flagshipConfig_test.go
+++ lib/flagshipConfig_test.go
@@ -10,16 +10,28 @@ import (
 func resetForEachTest(oldArgs []string) {
 	os.Args = oldArgs
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	_ = os.Unsetenv(FS_BUCKETING_DIRECTORY)
+
 	_ = os.Unsetenv(FS_ENV_ID)
 	_ = os.Unsetenv(FS_POLLING_INTERVAL)
+	_ = os.Unsetenv(FS_PORT)
+	_ = os.Unsetenv(FS_ADDRESS)
 }
 
 func messageError(t *testing.T, name string, expected interface{}, result interface{}) {
 	t.Errorf(name+" failed, expected %v but got %v", expected, result)
 }
 
-func TestNewEnvId(t *testing.T) {
+func TestEnvID(t *testing.T) {
+	var flagshipConfig FlagshipConfig
+	oldArgs := os.Args
+	defer resetForEachTest(oldArgs)
+	_, err := flagshipConfig.New()
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestNewDefault(t *testing.T) {
 	var flagshipConfig FlagshipConfig
 	oldArgs := os.Args
 	defer resetForEachTest(oldArgs)
@@ -28,84 +40,49 @@ func TestNewEnvId(t *testing.T) {
 	envId := "envId_id"
 	os.Args = []string{"cmd", "-envId=" + envId}
 	flagshipConfig.New()
-	if os.Getenv(FS_ENV_ID) != envId {
+
+	if flagshipConfig.EnvId != envId {
+		t.Fail()
+	}
+
+	if flagshipConfig.PollingInterval != DEFAULT_POLLING_INTERVAL {
+		t.Fail()
+	}
+
+	if flagshipConfig.Port != DEFAULT_PORT {
+		t.Fail()
+	}
+
+	if flagshipConfig.Address != DEFAULT_ADDRESS {
 		t.Fail()
 	}
 }
-
-func TestNewPollingLong(t *testing.T) {
+func TestNew(t *testing.T) {
 	var flagshipConfig FlagshipConfig
 	oldArgs := os.Args
 	defer resetForEachTest(oldArgs)
 
 	//test FLAGSHIP_ENV_ID
+	envId := "envId_id"
 	pollingInterval := 5200
-	os.Args = []string{"cmd", "-pollingInterval=" + strconv.Itoa(pollingInterval)}
+	port := 3001
+	address := "127.0.0.1"
+	os.Args = []string{"cmd", "-envId=" + envId, "-pollingInterval=" + strconv.Itoa(pollingInterval), "-port=" + strconv.Itoa(port), "-address=" + address}
 	flagshipConfig.New()
-	if os.Getenv(FS_POLLING_INTERVAL) != strconv.Itoa(pollingInterval) {
-		t.Fail()
-	}
-}
-
-func TestNewBucketingDirectoryLong(t *testing.T) {
-	var flagshipConfig FlagshipConfig
-	oldArgs := os.Args
-	defer resetForEachTest(oldArgs)
-
-	//Test FLAGSHIP_BUCKETING_DIRECTORY
-	bucketingDirectory := "flagshipDirectory"
-	os.Args = []string{"cmd", "-bucketingPath=" + bucketingDirectory}
-	flagshipConfig = FlagshipConfig{}
-	flagshipConfig.New()
-	if os.Getenv(FS_BUCKETING_DIRECTORY) != bucketingDirectory {
-		t.Fail()
-	}
-}
-
-func TestGetConfigFromEnv(t *testing.T) {
-	var flagshipConfig FlagshipConfig
-	oldArgs := os.Args
-	defer resetForEachTest(oldArgs)
-	bucketingDirectory := "flagshipDirectory"
-	pollingInterval := 5200
-	envId := "env_id"
-
-	_ = os.Setenv(FS_ENV_ID, envId)
-	_ = os.Setenv(FS_POLLING_INTERVAL, strconv.Itoa(pollingInterval))
-	_ = os.Setenv(FS_BUCKETING_DIRECTORY, bucketingDirectory)
-
-	_, err := flagshipConfig.GetConfig()
-
-	if err != nil {
-		t.Fail()
-	}
 
 	if flagshipConfig.EnvId != envId {
-		messageError(t, "flagshipConfig.EnvId", envId, flagshipConfig.EnvId)
+		t.Fail()
 	}
 
 	if flagshipConfig.PollingInterval != pollingInterval {
-		messageError(t, "flagshipConfig.PollingInterval", pollingInterval, flagshipConfig.PollingInterval)
+		t.Fail()
 	}
 
-	if flagshipConfig.BucketingPath != bucketingDirectory {
-		messageError(t, "flagshipConfig.BucketingPath", bucketingDirectory, flagshipConfig.BucketingPath)
-	}
-}
-
-//Test without envId
-func TestGetConfigFromEnv1(t *testing.T) {
-	var flagshipConfig FlagshipConfig
-	oldArgs := os.Args
-	defer resetForEachTest(oldArgs)
-
-	_, err := flagshipConfig.GetConfig()
-
-	if err == nil {
-		t.FailNow()
+	if flagshipConfig.Port != port {
+		t.Fail()
 	}
 
-	if err.Error() != FlagshipEnvIdErrorMessage {
+	if flagshipConfig.Address != address {
 		t.Fail()
 	}
 }
